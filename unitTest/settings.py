fd7ed99414838af7e0780397e2c93b5bd73bb6d6
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2022-02-23 14:54:55 +0100

    Secret Hidden

--- unitTest/settings.py
+++ unitTest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s9r*jxfai+8%i6@+vjl$(9-#s6c-5^$yt82ddm00&izj$^8$34'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
